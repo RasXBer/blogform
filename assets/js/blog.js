7b21adfe0a740e4141ceaab16114b0cd80644686
rasika <rasikab@yahoo.com>
2024-03-11 00:12:33 +1100

    blog css fixes

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,34 +1,37 @@
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     // Retrieve data from local storage
     var userData = JSON.parse(localStorage.getItem("myForm"));
-for (let i = 0; i < userData.length; i++) {
-    
-    
+    for (let i = 0; i < userData.length; i++) {
 
-    // Log local storage data
-    console.log("User Name:", userData[i].userName);
-    console.log("Title:", userData[i].title);
-    console.log("Content:", userData[i].content);
-// Log local storage data on My First Blog page
-const blogPostsContainer = document.getElementById('blogPosts');
-   const userInfo= document.createElement('h2')
-   const titleInfo= document.createElement('h3')
-   const contentInfo= document.createElement('h4')
-   userInfo.textContent=userData[i].userName;
-   titleInfo.textContent=userData[i].title;
-   contentInfo.textContent=userData[i].content;
-   blogPostsContainer.appendChild(userInfo);
-   blogPostsContainer.appendChild(titleInfo);
-   blogPostsContainer.appendChild(contentInfo);
-} 
+
+
+        // Log local storage data
+        console.log("User Name:", userData[i].userName);
+        console.log("Title:", userData[i].title);
+        console.log("Content:", userData[i].content);
+        // Log local storage data on My First Blog page
+        const blogPostsContainer = document.getElementById('blogPosts');
+        const userInfo = document.createElement('div')
+        const titleInfo = document.createElement('div')
+        const contentInfo = document.createElement('div')
+        //    userInfo.textContent=userData[i].userName;
+        userInfo.textContent = userData[i].userName;
+        titleInfo.textContent = userData[i].title;
+        contentInfo.textContent = userData[i].content;
+
+
+        blogPostsContainer.appendChild(titleInfo);
+        blogPostsContainer.appendChild(contentInfo);
+        blogPostsContainer.appendChild(userInfo);
+    }
 });
 
-document.querySelector("#backbtn").addEventListener("click", function(){
+document.querySelector("#backbtn").addEventListener("click", function () {
     document.location.replace("index.html")
 })
 
 function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
- }
+}
